79e151a57cb9abcc4927e905c4ea8ea319095b82
arikaim <info@arikaim.com>
2025-11-30 15:04:56 +0200

    =

--- ControlPanel.php
+++ ControlPanel.php
@@ -21,12 +21,15 @@ class ControlPanel extends Controller
      *
      * @param \Psr\Http\Message\ServerRequestInterface $request
      * @param \Psr\Http\Message\ResponseInterface $response
-     * @param Validator $data
-     * @return Psr\Http\Message\ResponseInterface
+     * @param \Arikaim\Core\Validator\Validator $data
+     * @return mixed
     */
     public function loadControlPanel($request, $response, $data) 
     {                 
         $this->get('access')->withProvider('session');
+
+        // Set contorl panel template as primary
+        $this->get('view')->setPrimaryTemplate('system');
 
         return $this->pageLoad($request,$response,$data,'system:admin');       
     }
